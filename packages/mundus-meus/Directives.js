80d4316f983c4bda8e1ff2a724084f040121ebaa
Wildhoney <adam@zephyrhealthinc.com>
2013-07-22 20:28:43 +0100

    Map now fits the bounds. Fixes #14

--- packages/mundus-meus/Directives.js
+++ packages/mundus-meus/Directives.js
@@ -67,6 +67,8 @@ app.directive('map', ['$mundusMeus', function($mundusMeus) {
          */
         $scope.$on('plotMarkers', function(context, models) {
 
+            var latLongBounds = [];
+
             // Clear all of the markers.
             $mundusMeus.highlightMarker();
             allMarkers.length = 0;
@@ -81,7 +83,12 @@ app.directive('map', ['$mundusMeus', function($mundusMeus) {
                 // Keep a track of the markers in the collection.
                 allMarkers.push({ model: model, marker: marker });
 
+                // Append each lat/long pair to calculate the bounds.
+                latLongBounds.push([model.latitude, model.longitude]);
+
             });
+
+            map.fitBounds([latLongBounds]);
 
         });
 
